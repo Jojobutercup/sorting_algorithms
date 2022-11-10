f2b06207a9596791e8ccf88cf01e8ede32c67286
Osariemen Uwuilehue <106764970+Jojobutercup@users.noreply.github.com>
2022-11-10 07:01:48 -0800

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,36 @@
-/*
- * File: 0-bubble_sort.c
- * Auth: Sangwani P Zyambo
- */
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * using the Bubble sort algorith.
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
  *
- * @array: Array of integers to be sorted
- * @size: The size of the @array
+ * Return: void
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
+	size_t i, n, new_n;
+	int swap;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL || size < 2)
+		return;
+
+	n = size;
+	while (n > 0)
 	{
-		for (j = 0; j < size - 1; j++)
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
 		{
-			/* if this pair is out of order */
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				/* swap them and remember something changed */
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
 				print_array(array, size);
 			}
 		}
+		n = new_n;
 	}
 }
